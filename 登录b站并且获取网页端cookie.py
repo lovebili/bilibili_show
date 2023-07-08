62c5b5956eb89a0f500bc2a298a948454c23b6d8
1486308032 <50775291+1486308032@users.noreply.github.com>
2023-07-08 16:34:28 +0800

    Update 登录b站并且获取网页端cookie.py

--- 登录b站并且获取网页端cookie.py
+++ 登录b站并且获取网页端cookie.py
@@ -18,6 +18,7 @@ web.delete_all_cookies()
 time.sleep(50)
 # 读取登录之后浏览器的cookies
 dictcookies = web.get_cookies()
+dic={}
 dic['bilibili_cookies']=[dictcookies]
 # 将字典数据转成json数据便于保存
 jsoncookies = json.dumps(dic)
