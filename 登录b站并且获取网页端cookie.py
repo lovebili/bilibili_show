604bebf56fdd4dfaf34e453afd818bf15a099ff6
Calm <50775291+1486308032@users.noreply.github.com>
2023-07-14 10:30:13 +0800

    Update 登录b站并且获取网页端cookie.py

--- 登录b站并且获取网页端cookie.py
+++ 登录b站并且获取网页端cookie.py
@@ -15,18 +15,23 @@ web.get('https://www.bilibili.com')
 # 先删除以前保存的cookies
 web.delete_all_cookies()
 # 这个时间用于手动登录,扫码登录可以适当缩短这个等待时间，默认时间为50秒
-time.sleep(50)
-# 读取登录之后浏览器的cookies
-dictcookies = web.get_cookies()
-dic={}
-dic['bilibili_cookies']=dictcookies
-# 将字典数据转成json数据便于保存
-jsoncookies = json.dumps(dic)
-# 写进文本保存
-with open('cookie.json','w') as f:
-    f.write(jsoncookies)
-print('cookies is ok')
+while True:
+    time.sleep(3)
+    try:
+        web.find_element(By.CLASS_NAME, "header-login-entry")
+    except:
+        # 读取登录之后浏览器的cookies
+        dictcookies = web.get_cookies()
+        dic = {}
+        dic['bilibili_cookies'] = dictcookies
+        # 将字典数据转成json数据便于保存
+        jsoncookies = json.dumps(dic)
+        # 写进文本保存
+        with open('cookie.json', 'w') as f:
+            f.write(jsoncookies)
+        print('cookies is ok')
+        break
 
 web.get('https://www.bilibili.com/bangumi/play/ep316326/?share_source=copy_web&t=10921')
 
-sleep(131003)
+sleep(5)
