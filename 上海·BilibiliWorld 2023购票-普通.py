09bbe6051bbc1b1bd5465c930ddf9ce6538a48bc
Calm <50775291+1486308032@users.noreply.github.com>
2023-07-14 10:37:49 +0800

    Add files via upload

--- 上海·BilibiliWorld 2023购票-普通.py
+++ 上海·BilibiliWorld 2023购票-普通.py
@@ -10,18 +10,22 @@ from selenium.webdriver.support import expected_conditions as EC
 engine = pyttsx3.init()
 buy_url = "https://show.bilibili.com/platform/detail.html?id=73710&from=pc_ticketlist"
 
-day=int(input("\n请输入购票的日期，1表示21号，2表示22号，3表示23号\n"))
-user_lis=int(input("\n请输入购票人员身份序列号，以空格分隔（例如第1、3、4人则输入：1 3 4）："))
-user_list=user_lis.split(" ")
-print(day)
+day = int(input("\n请输入购票的日期，1表示21号，2表示22号，3表示23号\n"))
+user_lis = input("\n请输入购票人员身份序列号，以空格分隔（例如第1、3、4人则输入：1 3 4）：")
+user_list = user_lis.split(" ")
+for i in range(len(user_list)):
+    user_list[i] = int(user_list[i])
+
+print("{}:{}".format(day, user_list))
+
 # 设置购票的日期
-day_xpath  = f"/html/body/div/div[2]/div[2]/div[2]/div[2]/div[4]/ul[1]/li[2]/div["+str(day)+"]"
+day_xpath = f"/html/body/div/div[2]/div[2]/div[2]/div[2]/div[4]/ul[1]/li[2]/div[" + str(day) + "]"
 # 该日期为21日票 /html/body/div/div[2]/div[2]/div[2]/div[2]/div[4]/ul[1]/li[2]/div[1]
 
 # 在这里设置购票的种类
 tick_xpath = f"/html/body/div/div[2]/div[2]/div[2]/div[2]/div[4]/ul[2]/li[2]/div[3]"
 
-#/html/body/div/div[2]/div[2]/div[2]/div[2]/div[4]/ul[2]/li[2]/div[4]这个是780元的价格的c区
+# /html/body/div/div[2]/div[2]/div[2]/div[2]/div[4]/ul[2]/li[2]/div[4]这个是780元的价格的c区
 
 # 加载配置文件
 with open('./cookie.json', 'r') as f:
@@ -35,6 +39,7 @@ def voice(message):
     engine.stop()
     voice(message)
 
+
 # 设置抢购时间
 TargetTime = "2013-10-3 8:00:00.00000000"
 
@@ -42,8 +47,7 @@ WebDriver = webdriver.Chrome()
 wait = WebDriverWait(WebDriver, 0.5)
 if len(config["bilibili_cookies"]) == 0:
     # 输入目标购买页面
-    WebDriver.get(
-        buy_url)
+    WebDriver.get(buy_url)
     WebDriver.maximize_window()
     time.sleep(1)
     WebDriver.find_element(By.CLASS_NAME, "nav-header-register").click()
@@ -58,27 +62,14 @@ if len(config["bilibili_cookies"]) == 0:
     with open('./cookie.json', 'w') as f:
         json.dump(config, f, indent=4)
 else:
-    WebDriver.get(
-        buy_url)  # 输入目标购买页面
+    WebDriver.get(buy_url)  # 输入目标购买页面
+    # 弹窗按钮
+    try:
+        WebDriver.find_element(By.CLASS_NAME, "real-name-modal-confirm-btn").click()
+    except:
+        pass
     for cookie in config["bilibili_cookies"]:  # 利用cookies登录账号
-        my_cookie = {
-            'domain': cookie['domain'],
-            'name': cookie['name'],
-            'value': cookie['value'],
-            'path': cookie['path'],
-        }
-        if 'expiry' in cookie:
-            cookie['expiry'] = cookie['expiry']
-        if 'httpOnly' in cookie:
-            cookie['httpOnly'] = cookie['httpOnly']
-        if 'sameSite' in cookie:
-            cookie['sameSite'] = cookie['sameSite']
-        if 'secure' in cookie:
-            cookie['secure'] = cookie['secure']
-
-        WebDriver.add_cookie(
-            my_cookie
-        )
+        WebDriver.add_cookie(cookie)
         WebDriver.maximize_window()
 
 while True:
@@ -94,15 +85,15 @@ while True:
             element = wait.until(EC.visibility_of_element_located(
                 (By.XPATH, day_xpath)))
             element.click()
-            #增加购买人数
+            # 增加购买人数
             element = wait.until(EC.visibility_of_element_located(
                 (By.CLASS_NAME, "count-plus")))
             for i in range(len(user_list)):
                 element.click()
 
-            #element = wait.until(EC.visibility_of_element_located(
-                #(By.XPATH, tick_xpath)))
-            #element.click()
+            # element = wait.until(EC.visibility_of_element_located(
+            # (By.XPATH, tick_xpath)))
+            # element.click()
         # 等待抢票按钮出现并可点击
         element = wait.until(EC.element_to_be_clickable(
             (By.CLASS_NAME, 'product-buy.enable')))
@@ -115,20 +106,20 @@ while True:
     except:
         WebDriver.refresh()
         continue
-#选择购票人
+# 选择购票人
 while True:
     try:
-        user_str='card-item-container'
+        user_str = 'card-item-container'
         element = wait.until(EC.visibility_of_element_located(
-                        (By.CLASS_NAME, user_str)))
+            (By.CLASS_NAME, user_str)))
 
-        elements=WebDriver.find_elements(By.CLASS_NAME, "card-item-container")
+        elements = WebDriver.find_elements(By.CLASS_NAME, "card-item-container")
         # for i in elements:
         #     if not i.find_elements(By.CLASS_NAME, "icon-checked"):
         #         i.click()
         for i in user_list:
-            i=int(i)-1
-            elements=WebDriver.find_elements(By.CLASS_NAME, "card-item-container")[i]
+            i = int(i) - 1
+            elements = WebDriver.find_elements(By.CLASS_NAME, "card-item-container")[i]
             elements.click()
         break
     except:
@@ -138,7 +129,7 @@ while True:
 element = None
 while element is None:
     try:
-        element=WebDriver.find_element(By.CLASS_NAME, "confirm-paybtn.active")
+        element = WebDriver.find_element(By.CLASS_NAME, "confirm-paybtn.active")
     except:
         continue
 element.click()
@@ -147,8 +138,9 @@ voice('二次元，你的票买到了，快付款')
 # 语音提醒时间
 time.sleep(60)
 while True:
-    try:# 确认协议按钮iki
-        quereng_button = WebDriver.find_elements(By.XPATH, "/html/body/div/div[2]/div/div[5]/div[1]/div/span[1]").click()
+    try:  # 确认协议按钮iki
+        quereng_button = WebDriver.find_elements(By.XPATH,
+                                                 "/html/body/div/div[2]/div/div[5]/div[1]/div/span[1]").click()
         # 确认支付按钮
         quereng_zhifu_button = WebDriver.find_elements(By.XPATH, "/html/body/div/div[2]/div/div[5]/div[2]").click()
         print("该付款了")
